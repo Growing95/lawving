6280d989bc9712d0a68f0f80623e4cb87b044994
ict02-03 <ict02-03@ict02-03>
2021-04-08 15:17:11 +0900

    1

--- src/main/java/com/ict/lawving/notice/controller/NoticeController.java
+++ src/main/java/com/ict/lawving/notice/controller/NoticeController.java
@@ -1,5 +1,39 @@
 package com.ict.lawving.notice.controller;
 
-public class NoticeController {
+import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ict.lawving.notice.model.service.NoticeService;
+import com.ict.lawving.notice.model.vo.NoticeSearch;
+import com.ict.lawving.notice.model.vo.NoticeVo;
+
+@Controller
+public class NoticeController {
+	private static final Logger logger = LoggerFactory.getLogger("NoticeController.class");
+
+	@Autowired
+	private NoticeService noticeService;
+
+	@RequestMapping(value = "nsearch.do", method = RequestMethod.POST)
+	public String noticeSearchTitleMethod(@RequestParam("category") String category,
+			@RequestParam("order") String order, @RequestParam("keyword") String keyword, Model model) {
+		NoticeSearch searchObject = new NoticeSearch(category, order, keyword);
+		ArrayList<NoticeVo> list = noticeService.selectSearch(searchObject);
+
+		if (list.size() > 0) {
+			model.addAttribute("list", list);
+			return "notice/noticeListView";
+		} else {
+			model.addAttribute("msg", keyword + "로 검색된 공지사항 정보가 없습니다.");
+			return "common/errorPage";
+		}
+	}
 }
--- src/main/java/com/ict/lawving/notice/model/dao/NoticeDao.java
+++ src/main/java/com/ict/lawving/notice/model/dao/NoticeDao.java
@@ -1,5 +1,34 @@
+
 package com.ict.lawving.notice.model.dao;
 
-public class NoticeDao {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ict.lawving.notice.model.vo.NoticeSearch;
+import com.ict.lawving.notice.model.vo.NoticeVo;
+
+
+@Repository("noticeDao")
+public class NoticeDao {
+// 스프링-마이바티스 연동 객체 연결함 : root-context.xml 에 선언되어 있음
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+
+	// Notice 목록 조회하기
+		public ArrayList<NoticeVo> selectNoticeList() {
+			List<NoticeVo> NoticeList = sqlSession.selectList("noticeMapper.list");
+			return (ArrayList<NoticeVo>) NoticeList;
+		}
+
+
+		public ArrayList<NoticeVo> selectSearch(NoticeSearch searchObject) {
+			List<NoticeVo> NoticeList = sqlSession.selectList("noticeMapper.search",searchObject);
+			return (ArrayList<NoticeVo>) NoticeList;
+		}
+
+	}
